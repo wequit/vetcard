6ce0f7fa1e5ecb22bdb7f96c8833418a6009dbe4
weauitt <adilhanimasev6@gmail.com>
2025-06-20 14:46:54 +0600

    for test

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -3,6 +3,7 @@ import { HomePage } from '@/pages/public/home/ui/HomePage';
 import { AboutPage } from '@/pages/public/about/AboutPage'; 
 import RegisterForm  from '@/pages/public/auth/register/ui/RegisterPage'; 
 import { Soon } from '@/pages/private/page';
+import { NotFoundPage } from '@/pages/note-found/ui/NotFoundPage';
 
 export const Router = () => {
   return (
@@ -11,6 +12,7 @@ export const Router = () => {
       <Route path="/about" element={<AboutPage />} />
       <Route path="/login" element={<RegisterForm />} />
       <Route path="/dashboard" element={<Soon />} />
+      <Route path="*" element={<NotFoundPage />} />
     </Routes>
   );
 };
---
+++ src/pages/note-found/ui/NotFoundPage.tsx
@@ -0,0 +1,6 @@
+
+export const NotFoundPage = () => (
+    <div className="min-h-screen flex text-center justify-center items-center bg-white ">
+     <h1 className=" text-center text-3xl">404 Not Found</h1>
+    </div>
+  );
