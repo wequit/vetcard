d2bf14641ad6971c03376854c599410ea6acc835
Aidanek <kadyrovaidanek@gmail.com>
2025-06-21 16:59:30 +0600

    Страница для о нас

--- src/pages/public/home/ui/Header/Header.tsx
+++ src/pages/public/home/ui/Header/Header.tsx
@@ -2,7 +2,7 @@ import { Logo } from "@/shared/ui/Logo";
 import { NavLink } from "./NavLink";
 
 export const Header = () => (
-  <header className="container mx-auto px-6 py-3 flex justify-between items-center border-b border-gray-200">
+  <header className="container mx-auto px-6 py-3 flex justify-between items-center border-b ">
     <Logo />
     <nav className="flex gap-8">
       <NavLink to="/" isActive>Главная</NavLink>
